9539bc57aadc13e8107367e6dd55ae9ef192aae2
kent1 <kent1@arscenic.info>
2009-06-24 14:47:16 +0000

    On passe la plupart des styles des tooltips dans une class...
    Utile dans le cas où nous faisons un usage détourné des js de statitiques

--- javascript/jquery.tflot.js
+++ javascript/jquery.tflot.js
@@ -660,15 +660,10 @@
 	// montrer les informations des points
     function showTooltip(x, y, contents) {
         $('<div id="tooltip">' + contents + '</div>').css( {
-            position: 'absolute',
-            display: 'none',
             top: y + 5,
             left: x + 5,
-            border: '1px solid #fdd',
-            padding: '2px',
-            'background-color': '#fee',
             opacity: 0.80
-        }).appendTo("body").fadeIn(200);
+        }).addClass('tooltip_statistiques').appendTo("body").fadeIn(200);
     }
 
 
