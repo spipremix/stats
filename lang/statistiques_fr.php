b565f6b208bc14331fa89b36b5cf28d6a6b09e05
Cerdic <cedric@yterium.com>
2011-04-30 14:50:41 +0000

    initialiser le module de langue

---
+++ lang/statistiques_fr.php
@@ -0,0 +1,48 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/grenier/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_radio_sauvegarde_compressee' => 'sauvegarde compressée sous @fichier@',
+	'bouton_radio_sauvegarde_non_compressee' => 'sauvegarde non compressée sous @fichier@',
+
+	// I
+	'info_base_restauration' => 'La base est en cours de restauration.',
+	'info_erreur_restauration' => 'Erreur de restauration : fichier inexistant.',
+	'info_restauration_sauvegarde_insert' => 'Insertion de @archive@ dans la base',
+	'info_sauvegarde_articles' => 'Sauvegarder les articles',
+	'info_sauvegarde_articles_sites_ref' => 'Sauvegarder les articles des sites référencés',
+	'info_sauvegarde_auteurs' => 'Sauvegarder les auteurs',
+	'info_sauvegarde_breves' => 'Sauvegarder les brèves',
+	'info_sauvegarde_documents' => 'Sauvegarder les documents',
+	'info_sauvegarde_echouee' => 'Si la sauvegarde a échoué («Maximum execution time exceeded»),',
+	'info_sauvegarde_forums' => 'Sauvegarder les forums',
+	'info_sauvegarde_groupe_mots' => 'Sauvegarder les groupes de mots',
+	'info_sauvegarde_messages' => 'Sauvegarder les messages',
+	'info_sauvegarde_mots_cles' => 'Sauvegarder les mots-clés',
+	'info_sauvegarde_petitions' => 'Sauvegarder les pétitions',
+	'info_sauvegarde_refers' => 'Sauvegarder les referers',
+	'info_sauvegarde_reussi_01' => 'Sauvegarde réussie.',
+	'info_sauvegarde_rubrique_reussi' => 'Les tables de la rubrique @titre@ ont été sauvegardée dans @archive@. Vous pouvez',
+	'info_sauvegarde_rubriques' => 'Sauvegarder les rubriques',
+	'info_sauvegarde_signatures' => 'Sauvegarder les signatures de pétitions',
+	'info_sauvegarde_sites_references' => 'Sauvegarder les sites référencés',
+	'info_sauvegarde_type_documents' => 'Sauvegarder les types de documents',
+	'info_sauvegarde_visites' => 'Sauvegarder les visites',
+
+	// S
+	'sauvegarde_fusionner' => 'Fusionner la base actuelle et la sauvegarde',
+	'sauvegarde_fusionner_depublier' => 'Dépublier les objets fusionnés',
+	'sauvegarde_url_origine' => 'Eventuellement, URL du site d\'origine :',
+
+	// T
+	'texte_admin_tech_03' => 'Vous pouvez choisir de sauvegarder le fichier sous forme compressée, afin
+	d\'écourter son transfert chez vous ou sur un serveur de sauvegardes, et d\'économiser de l\'espace disque.',
+	'texte_admin_tech_04' => 'Dans un but de fusion avec une autre base, vous pouvez limiter la sauvegarde à la rubrique: ',
+	'texte_sauvegarde_compressee' => 'La sauvegarde sera faite dans le fichier non compressé @fichier@.'
+);
+
+?>
