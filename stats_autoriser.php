b78dca1082d084e27ace2deb25feff4f63fd9d1f
Cerdic <cedric@yterium.com>
2011-07-31 18:17:03 +0000

    http://core.spip.org/issues/2176 : autoriser('menu',xxx) au lieu de  autoriser('bouton',xxx)

--- stats_autoriser.php
+++ stats_autoriser.php
@@ -3,10 +3,10 @@
 function stats_autoriser(){}
 
 
-function autoriser_statistiques_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+function autoriser_statistiques_menu_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
 	return autoriser('voirstats', $type, $id, $qui, $opt);
 }
-function autoriser_referers_bouton_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
+function autoriser_referers_menu_dist($faire, $type='', $id=0, $qui = NULL, $opt = NULL){
 	return autoriser('voirstats', $type, $id, $qui, $opt);
 }
 
@@ -20,21 +20,21 @@ function autoriser_voirstats_dist($faire, $type, $id, $qui, $opt) {
 }
 
 // autorisation des boutons et onglets
-function autoriser_statistiques_visites_bouton_dist($faire, $type, $id, $qui, $opt) {
+function autoriser_statistiques_visites_menu_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('voirstats', $type, $id, $qui, $opt);
 }
 
-function autoriser_statistiques_repartition_bouton_dist($faire, $type, $id, $qui, $opt) {
+function autoriser_statistiques_repartition_menu_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('voirstats', $type, $id, $qui, $opt);
 }
 
-function autoriser_statistiques_lang_bouton_dist($faire, $type, $id, $qui, $opt) {
+function autoriser_statistiques_lang_menu_dist($faire, $type, $id, $qui, $opt) {
 	return ($GLOBALS['meta']['multi_articles'] == 'oui' 
 			OR $GLOBALS['meta']['multi_rubriques'] == 'oui')
 		AND autoriser('voirstats', $type, $id, $qui, $opt);
 }
 
-function autoriser_statistiques_referers_bouton_dist($faire, $type, $id, $qui, $opt) {
+function autoriser_statistiques_referers_menu_dist($faire, $type, $id, $qui, $opt) {
 	return autoriser('voirstats', $type, $id, $qui, $opt);
 }
 
