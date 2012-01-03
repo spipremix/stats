67dfd3cc858f48747917652e7d525453508d33ee
salvatore <salvatore@rezo.net>
2012-01-03 14:07:12 +0000

    langues (paquet-stats)

--- lang/paquet-stats_ar.php
+++ lang/paquet-stats_ar.php
@@ -1,18 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-stats
-// Langue: ar
-// Date: 03-08-2011 19:45:21
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
+	'stats_description' => 'Statistiques de SPIP', # NEW
 	'stats_nom' => 'الإحصاءات',
-	'stats_slogan' => 'إدارة الإحصاءات في SPIP',
+	'stats_slogan' => 'إدارة الإحصاءات في SPIP'
 );
+
 ?>
--- lang/paquet-stats_fr.php
+++ lang/paquet-stats_fr.php
@@ -1,19 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-stats
-// Langue: fr
-// Date: 03-08-2011 19:45:21
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/statistiques/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'stats_description' => 'Statistiques de SPIP',
 	'stats_nom' => 'Statistiques',
-	'stats_slogan' => 'Gestion des statistiques dans SPIP',
+	'stats_slogan' => 'Gestion des statistiques dans SPIP'
 );
+
 ?>
