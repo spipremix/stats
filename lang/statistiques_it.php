52744c2df601a8739b9a480137a97ef2bc663025
elvis.deltedesco <elvis.deltedesco@phante.com>
2012-03-03 10:43:24 +0000

    langues (statistiques)

--- lang/statistiques_it.php
+++ lang/statistiques_it.php
@@ -8,11 +8,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_effacer_referers' => 'Effacer seulement les liens entrants', # NEW
-	'bouton_effacer_statistiques' => 'Cancellare le statistiche', # MODIF
+	'bouton_effacer_referers' => 'Eliminare solo i collegamenti in entrata',
+	'bouton_effacer_statistiques' => 'Cancellare le statistiche',
 
 	// C
-	'csv' => 'csv', # NEW
+	'csv' => 'csv',
 
 	// I
 	'icone_evolution_visites' => 'Evoluzione delle visite<br />@visites@ visite',
@@ -25,13 +25,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_popularite_2' => 'popolarità del sito: ',
 	'info_popularite_3' => 'popolarità: @popularite@; visite: @visites@',
 	'info_popularite_5' => 'popolarità:',
-	'info_previsions' => 'prévisions :', # NEW
+	'info_previsions' => 'previsioni :',
 	'info_question_vignettes_referer' => 'Quando si consultano le statistiche è possibile visualizzare delle anteprime dei siti di origine delle visite',
 	'info_question_vignettes_referer_oui' => 'Visualizzare le catture di schermo dei siti di origine delle visite',
 	'info_visites' => 'visite:',
 	'info_visites_plus_populaires' => 'Pubblica le visite per <b>gli articoli più popolari </b> e per <b>gli ultimi articoli pubblicati:</b>',
 	'info_zoom' => 'zoom',
-	'item_gerer_statistiques' => 'Gestisci le statistiche', # MODIF
+	'item_gerer_statistiques' => 'Gestisci le statistiche delle visite',
 
 	// O
 	'onglet_origine_visites' => 'Origine delle visite',
@@ -39,11 +39,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'onglet_repartition_lang' => 'Suddivisione per lingua',
 
 	// R
-	'resume' => 'Resume', # MODIF
+	'resume' => 'Riassunto',
 
 	// T
 	'texte_admin_effacer_stats' => 'Questo comando cancella tutti i dati collegati alle statistiche delle visite al sito, comprese la popolarità degli articoli.',
-	'texte_admin_effacer_toutes_stats' => 'Le premier bouton supprime toutes les statistiques : visites, popularité des articles et liens entrants.', # NEW
+	'texte_admin_effacer_toutes_stats' => 'Il pulsante elimina tutte le statistiche: visite, popolarità degli articoli, e connessioni entranti.',
 	'texte_comment_lire_tableau' => 'Il posizionamento dell\'articolo,
 nella classifica per popolarità, è indicato qui
 sopra. La popolarità dell\'articolo (una stima del
@@ -58,8 +58,8 @@ quando si passa con il mouse sopra al titolo.',
 	'titre_page_statistiques_visites' => 'Statistiche delle visite',
 
 	// V
-	'visites_journalieres' => 'Nombre de visites par jour', # NEW
-	'visites_mensuelles' => 'Nombre de visites par mois' # NEW
+	'visites_journalieres' => 'Numero di visite per giorno',
+	'visites_mensuelles' => 'Numero di visite per mese'
 );
 
 ?>
