9a60e45eec2434f304a4f7b358eb0b7e2ba0a58e
salvatore <salvatore@rezo.net>
2012-03-13 22:36:23 +0000

    langues (statistiques)

--- lang/statistiques_ru.php
+++ lang/statistiques_ru.php
@@ -8,30 +8,30 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_effacer_referers' => 'Effacer seulement les liens entrants', # NEW
+	'bouton_effacer_referers' => 'Удалить информацию о источниках переходов',
 	'bouton_effacer_statistiques' => 'Очистить статистику',
 
 	// C
-	'csv' => 'csv', # NEW
+	'csv' => 'csv',
 
 	// I
 	'icone_evolution_visites' => 'Количество посещений<br />@visites@ ',
 	'icone_repartition_actuelle' => 'На данный момент',
 	'icone_repartition_visites' => 'Распределение посещений',
-	'icone_statistiques_visites' => 'Статистика',
+	'icone_statistiques_visites' => 'Статистика посещений',
 	'info_affichier_visites_articles_plus_visites' => 'Самые популярные статьи с начала работы сайта: </b>',
-	'info_comment_lire_tableau' => 'Как прочитать этот график',
-	'info_forum_statistiques' => 'История посещений',
+	'info_comment_lire_tableau' => 'Пояснения к графику',
+	'info_forum_statistiques' => 'Статистика посещений сайта',
 	'info_popularite_2' => 'популярность сайта:',
 	'info_popularite_3' => 'популярность: @popularite@; посещения: @visites@',
 	'info_popularite_5' => 'популярность:',
 	'info_previsions' => 'прогноз:',
-	'info_question_vignettes_referer' => 'Когда Вы принимаете во внимание статистику, Вы можете просмотреть текущие посещения сайтов. ',
-	'info_question_vignettes_referer_oui' => 'Показывать изображения главной страницы',
+	'info_question_vignettes_referer' => 'Система может самостоятельно делать скриншоты главных страниц сайтов, с которых к вам приходят посетители. Если вы включите эту опцию, то это изображение будет выводится рядом с ссылкой на этот сайт.',
+	'info_question_vignettes_referer_oui' => 'Разрешить формировать скриншоты сайтов',
 	'info_visites' => 'посещения:',
 	'info_visites_plus_populaires' => 'Показать статистику для <b> самых популярных </b> и <b>последних опубликованных статей:</b>',
 	'info_zoom' => 'увеличить',
-	'item_gerer_statistiques' => 'Вести статистику', # MODIF
+	'item_gerer_statistiques' => 'Сохранять информацию о посещениях',
 
 	// O
 	'onglet_origine_visites' => 'Источники переходов',
@@ -39,11 +39,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'onglet_repartition_lang' => 'Распределение по языкам',
 
 	// R
-	'resume' => 'Resume', # NEW
+	'resume' => 'Сводка',
 
 	// T
 	'texte_admin_effacer_stats' => 'Это команда удаляет все данные, которые связаны со статистикой посещений сайта,включая статистику по статьям. ',
-	'texte_admin_effacer_toutes_stats' => 'Le premier bouton supprime toutes les statistiques : visites, popularité des articles et liens entrants.', # NEW
+	'texte_admin_effacer_toutes_stats' => 'Первая кнопка удаляет всю статистику о: посещениях, популярности и источниках переходов.',
 	'texte_comment_lire_tableau' => 'Справа 
  показывается уровень 
   посещаемости статьи; черной линией отображается прогноз посещений (при условии 
@@ -54,12 +54,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'texte_signification' => 'Темные штрихи представляют накопленные записи (общее количество подразделов), светлые штрихи, представляют количество посещений для каждого раздела.',
 	'titre_evolution_visite' => 'Уровень посещений',
 	'titre_liens_entrants' => 'Источники переходов',
-	'titre_page_statistiques' => 'Статистика разделов',
+	'titre_page_statistiques' => 'Распределение по разделам',
 	'titre_page_statistiques_visites' => 'История посещений',
 
 	// V
-	'visites_journalieres' => 'Nombre de visites par jour', # NEW
-	'visites_mensuelles' => 'Nombre de visites par mois' # NEW
+	'visites_journalieres' => 'Количество посетителей (день)',
+	'visites_mensuelles' => 'Количество посетителей (месяц)'
 );
 
 ?>
