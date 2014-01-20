e3f009fb384354638526bcf069bc7c650cdc6a98
th.schmit+trad.spip <th.schmit+trad.spip@gmail.com>
2014-01-20 04:39:28 +0000

    [Salvatore] [source:_core_/plugins/statistiques/lang/ statistiques] Export depuis http://trad.spip.net de la langue fr_tu

--- lang/statistiques_fr_tu.php
+++ lang/statistiques_fr_tu.php
@@ -8,7 +8,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
-	'bouton_effacer_statistiques' => 'Effacer les statistiques', # MODIF
+	'bouton_effacer_referers' => 'Effacer seulement les liens entrants',
+	'bouton_effacer_statistiques' => 'Effacer toutes les statistiques',
+
+	// C
+	'csv' => 'csv',
 
 	// I
 	'icone_evolution_visites' => 'Évolution des visites<br />@visites@ visites',
@@ -21,12 +25,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_popularite_2' => 'popularité du site :',
 	'info_popularite_3' => 'popularité : @popularite@ ; visites : @visites@',
 	'info_popularite_5' => 'popularité :',
+	'info_previsions' => 'prévisions :',
 	'info_question_vignettes_referer' => 'Lorsque tu consultes les statistiques, tu peux visualiser des aperçus des sites d’origine des visites',
 	'info_question_vignettes_referer_oui' => 'Afficher les captures des sites d’origine des visites',
 	'info_visites' => 'visites :',
 	'info_visites_plus_populaires' => 'Afficher les visites pour <b>les articles les plus populaires</b> et pour <b>les derniers articles publiés :</b>',
 	'info_zoom' => 'zoom',
-	'item_gerer_statistiques' => 'Gérer les statistiques', # MODIF
+	'item_gerer_statistiques' => 'Gérer les statistiques des visites',
 
 	// O
 	'onglet_origine_visites' => 'Origine des visites',
@@ -34,16 +39,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'onglet_repartition_lang' => 'Répartition par langues',
 
 	// R
-	'resume' => 'Resume', # MODIF
+	'resume' => 'Résumé',
 
 	// T
 	'texte_admin_effacer_stats' => 'Cette commande efface toutes les données liées aux statistiques de visite du site, y compris la popularité des articles.',
+	'texte_admin_effacer_toutes_stats' => 'Le premier bouton supprime toutes les statistiques : visites, popularité des articles et liens entrants.',
 	'texte_comment_lire_tableau' => 'Le rang de l’article, dans le classement par popularité, est indiqué dans la marge ; la popularité de l’article (une estimation du nombre de visites quotidiennes qu’il recevra si le rythme actuel de consultation se maintient) et le nombre de visites reçues depuis le début sont affichés dans la bulle qui apparaît lorsque la souris survole le titre.',
 	'texte_signification' => 'Les barres rouges représentent les entrées cumulées (total des sous-rubriques), les barres vertes le nombre de visites pour chaque rubrique.',
 	'titre_evolution_visite' => 'Évolution des visites',
 	'titre_liens_entrants' => 'Les liens entrants du jour',
 	'titre_page_statistiques' => 'Statistiques par rubriques',
-	'titre_page_statistiques_visites' => 'Statistiques des visites'
+	'titre_page_statistiques_visites' => 'Statistiques des visites',
+
+	// V
+	'visites_journalieres' => 'Nombre de visites par jour',
+	'visites_mensuelles' => 'Nombre de visites par mois'
 );
 
 ?>
