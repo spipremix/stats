c5e764f80d6711c59481310b85c463c836a8abfc
spip <spip@oliver-fr.org>
2015-03-17 04:34:15 +0000

    [Salvatore] [source:_core_/plugins/statistiques/lang/ statistiques] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/statistiques_oc_ni_mis.php
@@ -0,0 +1,51 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/statistiques?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'archiver' => 'Arquivà',
+	'archiver_et_nettoyer' => 'Arquivà e netegià',
+	'archiver_nettoyer' => 'Netegià',
+	'archiver_nettoyer_visites_articles' => 'Netegià li visita d’article',
+
+	// B
+	'bouton_effacer_referers' => 'Escassà basta lu estac entrant',
+	'bouton_effacer_statistiques' => 'Escassà touti li estatìstica',
+
+	// C
+	'csv' => 'csv',
+
+	// I
+	'icone_evolution_visites' => 'Evoulucioun dei visita<br />@visites@ visita',
+	'icone_repartition_actuelle' => 'Afichà la reparticioun atuala',
+	'icone_repartition_visites' => 'Reparticioun dei visita',
+	'icone_statistiques_visites' => 'Estatìstica',
+	'info_comment_lire_tableau' => 'Couma lire aqueu tabléu',
+	'info_forum_statistiques' => 'Estatìstica dei visita',
+	'info_popularite_2' => 'poupularità dóu sit :',
+	'info_popularite_3' => 'poupularità : @popularite@ ; visita : @visites@',
+	'info_popularite_5' => 'poupularità :',
+	'info_tableaux' => 'Tabléu',
+	'info_visites' => 'visita :',
+	'info_zoom' => 'zoom',
+	'item_gerer_statistiques' => 'Gerà li estatistica dei visita',
+
+	// O
+	'onglet_repartition_lang' => 'Reparticioun per lenga',
+
+	// R
+	'resume' => 'Coumpèndi',
+
+	// T
+	'titre_evolution_visite' => 'Evoulucioun dei visita',
+	'titre_liens_entrants' => 'Estac intrant',
+	'titre_page_statistiques' => 'Estatistica per rùbrica',
+	'titre_page_statistiques_visites' => 'Estatistica dei visita'
+);
+
+?>
