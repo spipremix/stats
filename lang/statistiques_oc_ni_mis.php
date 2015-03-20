64628e6ebe52787dbed9195d1222693366db60a1
spip <spip@oliver-fr.org>
2015-03-20 04:33:11 +0000

    [Salvatore] [source:_core_/plugins/statistiques/lang/ statistiques] Export depuis http://trad.spip.net de la langue oc_ni_mis

--- lang/statistiques_oc_ni_mis.php
+++ lang/statistiques_oc_ni_mis.php
@@ -17,9 +17,11 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'archiver_description' => 'Aquela pàgina fournisse lu óutis da netegià o arquivà li estatìstica dóu sit.',
 	'archiver_et_nettoyer' => 'Arquivà e netegià',
 	'archiver_nettoyer' => 'Netegià',
-	'archiver_nettoyer_referers_articles' => 'Netegià li referença ai article',
+	'archiver_nettoyer_explications' => 'Leva li ligna de referençaire o de visita dai article que noun esiston (o esiston pu) en la basa de dounada.',
+	'archiver_nettoyer_referers_articles' => 'Netegià li referençaire dai article',
 	'archiver_nettoyer_visites_articles' => 'Netegià li visita d’article',
 	'archiver_nombre_lignes' => 'Noumbre de ligna',
+	'archiver_operation_longue' => 'Aquela ouperacioun pòu estre fouòrça lònga, subretout dóu tems de la premièra esecucioun.',
 	'archiver_operations_irreversibles' => 'Aqueli ouperacioun soun ireversìbli !',
 
 	// B
@@ -34,6 +36,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_repartition_actuelle' => 'Afichà la reparticioun atuala',
 	'icone_repartition_visites' => 'Reparticioun dei visita',
 	'icone_statistiques_visites' => 'Estatìstica',
+	'info_affichier_visites_articles_plus_visites' => 'Afichà li visita per <b>lu article lu mai visitat despì lou principi :</b>',
 	'info_comment_lire_tableau' => 'Couma lire aqueu tabléu',
 	'info_forum_statistiques' => 'Estatìstica dei visita',
 	'info_graphiques' => 'Gràficou',
@@ -41,19 +44,27 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_popularite_3' => 'poupularità : @popularite@ ; visita : @visites@',
 	'info_popularite_5' => 'poupularità :',
 	'info_previsions' => 'previsioun :',
+	'info_question_vignettes_referer' => 'Coura counsultàs li estatìstica, poudès visualisà lu aperçut dei sit d’orìgine dei visita',
+	'info_question_vignettes_referer_oui' => 'Afichà li catura dei sit d’orìgine dei visita',
 	'info_tableaux' => 'Tabléu',
 	'info_visites' => 'visita :',
+	'info_visites_plus_populaires' => 'Afichà li visita per <b>lu article lu mai poupulari</b> e per <b>lu darrié article publicat :</b>',
 	'info_zoom' => 'zoom',
 	'item_gerer_statistiques' => 'Gerà li estatistica dei visita',
 
 	// O
 	'onglet_origine_visites' => 'Orìgine dei visita',
+	'onglet_repartition_debut' => 'despì lou principi',
 	'onglet_repartition_lang' => 'Reparticioun per lenga',
 
 	// R
 	'resume' => 'Coumpèndi',
 
 	// T
+	'texte_admin_effacer_stats' => 'Aquela coumanda escassa touti li dounada ligadi ai estatìstica de visita dóu sit, embé la poupularità dei article.',
+	'texte_admin_effacer_toutes_stats' => 'Lou premié boutoun suprima touti li esttìstica : visita, poupularità dei article e estac entant.',
+	'texte_comment_lire_tableau' => 'Lou ran de l’article, en lou classamen per poupularità, es endicat en la marge ; la poupularità de l’article (una estimacioun dóu noumbre de visita quoutidiana que recevrà se lou rìtoumou atual de counsultacioun si mantengue) e lou noumbre de visita reçudi despì lou principi soun afichat en la boufa que pareisse coura la rateta survoula lou titre.',
+	'texte_signification' => 'Li barra founçadi rapresenton li intrada cumuladi (toutal dei souta rùbrica), li barra clara lou noumbre de visita per cada rùbrica.',
 	'titre_evolution_visite' => 'Evoulucioun dei visita',
 	'titre_liens_entrants' => 'Estac intrant',
 	'titre_page_statistiques' => 'Estatistica per rùbrica',
