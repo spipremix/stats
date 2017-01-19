499d552139a18e2917ebb3335a4b548195f04595
Matthieu Marcillaud <marcimat@rezo.net>
2017-01-19 09:34:10 +0000

    Report partiel de r101820 : éviter une mauvaise écriture du fichier de visite lorsqu'il n'existe pas au préalable.

--- public/stats.php
+++ public/stats.php
@@ -109,6 +109,10 @@ function public_stats_dist($contexte = null, $referer = null) {
 	if (lire_fichier($fichier, $content)) {
 		$content = @unserialize($content);
 	}
+	// fichier absent probablement (ou problème unserialize)
+	if (!is_array($content)) {
+		$content = array();
+	}
 
 	// 2. Plafonner le nombre de hits pris en compte pour un IP (robots etc.)
 	// et ecrire la session
