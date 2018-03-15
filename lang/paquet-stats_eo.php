599297830222f058e5bc3aa8f9ef8159f41daedd
gpl <gpl@long-player.net>
2018-03-15 04:42:15 +0000

    [Salvatore] [source:_core_/plugins/statistiques/lang/ paquet-stats] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-stats_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-stats?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'stats_description' => 'Statistikoj de SPIP',
+	'stats_nom' => 'Statistikoj',
+	'stats_slogan' => 'Mastrumado de la statistikoj en SPIP'
+);
